f2a15f2317f736bf1b5b194e8d0ea345443fe7fd
CrisMrtz01 <kooper8273@gmail.com>
2020-05-20 20:19:47 -0600

    Segundo Archivo PHP

---
+++ Ejercicio2pg31.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 2 pg 31</title>
+</head>
+<body>
+		<center>
+     <div class = "card-panel purple lighten-3">
+		<h2>Calculo de Nomina de trabajador</h2>
+		<form action="EjercicioDospg31.php" method="post">
+			<label for="canthoras"><h2>Ingrese la cantidad de horas trabajadas al dia</h2></label>
+			<input type="text" name="canthoras" id="canthoras">
+			<input type="submit" value="Enviar datos" id="boton">
+		</form>	</center>
+	</div>
+</body>
+</html>
